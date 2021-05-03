bb4ea8c0fc4f9cc0b917719d3d83ba04f4b7e5ed
next-fire-starter <next.fire.starter@gmail.com>
2021-05-03 22:29:40 +0900

    Create table.sql

---
+++ env/db/table.sql
@@ -0,0 +1,11 @@
+-- テーブル定義
+
+-- メモ
+
+DROP TABLE IF EXISTS techsc.memo;
+CREATE TABLE IF NOT EXISTS techsc.memo ( 
+  idmemo NUMBER NOT NULL,
+  title VARCHAR(20) NOT NULL, 
+  note TEXT NOT NULL, 
+  PRIMARY KEY (idmemo)
+) WITHOUT OIDS;
