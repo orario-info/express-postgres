10ff4d5fe72306d5144ea95397fa9a976b0967d2
next-fire-starter <next.fire.starter@gmail.com>
2021-05-03 22:30:34 +0900

    Create initdata.sql

---
+++ env/db/initdata.sql
@@ -0,0 +1,5 @@
+--初期データ メモ
+INSERT INTO techsc.memo (idmemo, title, note) VALUES (1, '買い物', '歯ブラシと洗濯洗剤');
+INSERT INTO techsc.memo (idmemo, title, note) VALUES (2, '打合せ予定', 'パワポ資料の用意');
+INSERT INTO techsc.memo (idmemo, title, note) VALUES (3, '週末の予定1', '洗車');
+INSERT INTO techsc.memo (idmemo, title, note) VALUES (4, '週末の予定2', 'クリーニング');
